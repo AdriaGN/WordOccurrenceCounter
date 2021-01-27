ba1df08ce8605201095e9a10089c1965d4f61f4e
Adrià Grau Navacerrada <graunava@emea.roche.com>
2021-01-27 17:10:33 +0100

    Created a Domain File to preserver a data structure for the analyzed files

---
+++ Domain.FileModelType/File.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Domain.FileModelType
+{
+    public class File
+    {
+        public string Name { get; set; }
+
+        public Dictionary<string, int> WordOccurrences { get; set; }
+    }
+}
