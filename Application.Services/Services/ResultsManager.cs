e997d46097310d66cafe12af151ca64c9b11332d
Adrià Grau Navacerrada <graunava@emea.roche.com>
2021-01-28 21:35:56 +0100

    Defined a contract for IResultsManager and implemented it

--- Application.Services/Services/ResultsManager.cs
+++ Application.Services/Services/ResultsManager.cs
@@ -1,8 +1,46 @@
-﻿using Application.Services.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Domain.FileModelType;
+using Application.Services.Interfaces;
 
 namespace Application.Services
 {
     public class ResultsManager : IResultsManager
     {
+        public void ShowResults(string wordToFind, List<FileToCount> listOfCountedFiles)
+        {
+            var filesWithOccurrences = this.CountResults(wordToFind, listOfCountedFiles);
+
+            var orderedMatchingFiles = this.OrderTopFilesWithOccurrences(filesWithOccurrences);
+
+            foreach (var file in orderedMatchingFiles)
+            {
+                Console.Write(file.Key + ": " + file.Value);
+            }
+        }
+
+        public Dictionary<string, int> CountResults(string wordToFind, List<FileToCount> listOfCountedFiles)
+        {
+            Dictionary<string, int> dictOfMatchingFiles = new Dictionary<string, int>();
+
+            foreach (var file in listOfCountedFiles)
+            {
+                if (file.WordOccurrences.ContainsKey(wordToFind))
+                {
+                    dictOfMatchingFiles.Add(file.Name, file.WordOccurrences["wordToFind"]);
+                }
+            }
+
+            return dictOfMatchingFiles;
+        }
+
+        public IEnumerable<KeyValuePair<string, int>> OrderTopFilesWithOccurrences(Dictionary<string, int> dictOfMatchingFiles)
+        {
+            var orderedResults =
+                (from resultFile in dictOfMatchingFiles orderby resultFile.Value descending select resultFile).Take(10);
+
+            return orderedResults;
+        }
     }
 }
