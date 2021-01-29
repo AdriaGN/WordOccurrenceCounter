08d4775ab0a9dab4af8ab34c267ca5fc7131960d
Adrià Grau Navacerrada <graunava@emea.roche.com>
2021-01-29 15:17:39 +0100

    Implemented the main program Console cycle and upgraded the regex to allow new lines

--- WordOccurrenceCounter/Program.cs
+++ WordOccurrenceCounter/Program.cs
@@ -1,15 +1,37 @@
-﻿namespace Presentation.UI
+﻿using System;
+using Unity;
+using Application.Services;
+using Application.Services.Interfaces;
+
+namespace Presentation.UI
 {
     class Program
     {
         static void Main(string[] args)
         {
+            IUnityContainer container = new UnityContainer();
+            container.RegisterType<IFileManager, FileManager>();
+            container.RegisterType<IFileTextParser, FileTextParser>();
+            container.RegisterType<IResultsManager, ResultsManager>();
+
             var filesPath = args[0];
-            //filesToParse = FileManager.DiscoverFiles(filesPath);
-            //Console.log("Put your word here: ");
-            //word = readfromconsole();
-            //results = FileParser.findWordInFiles(filesToParse, word);
-            //Print("results")
+
+            var filesToParse = container.Resolve<IFileManager>();
+            var countedOccurrences = filesToParse.GetFileWordsOccurrencesCounted(filesPath);
+
+            string inputWord = string.Empty;
+
+            while (inputWord != ":q!")
+            {
+                Console.WriteLine("Put your word here: ");
+                inputWord = Console.ReadLine();
+
+                if (inputWord != ":q!")
+                {
+                    var results = container.Resolve<IResultsManager>();
+                    results.ShowResults(inputWord?.ToLower(), countedOccurrences);
+                }
+            }
         }
     }
 }
